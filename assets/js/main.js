956ffe3ae43bf0c53408b2149f30ddcd413207d8
Luan Costa <112595656+luuwn@users.noreply.github.com>
2023-09-21 05:21:01 +0000

    Update main.js, manipulando result da requisição

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,8 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 
 //chamando uma requisição do servidor da api
-fetch(url).then(function (response){
-    console.log(response);
-})
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch( (error) => console.error(error))
 
